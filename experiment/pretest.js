b26ebaefef247427b5115e7a3844d0cd5fd52074
Virtual Labs IIT Delhi <71967438+vlabsdev-iitd@users.noreply.github.com>
2021-09-21 14:44:39 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -89,7 +89,7 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. This is a sample question:", ///// Write the question inside double quotes
             answers: {
                 a: "This is a sample answer A", ///// Write the option 1 inside double quotes
                 b: "This is a sample answer B", ///// Write the option 2 inside double quotes
@@ -100,7 +100,7 @@
         },
 
     {
-      question: "2.This is a sample question:",  ///// Write the question inside double quotes
+      question: "2. This is a sample question:",  ///// Write the question inside double quotes
       answers: {
                 a: "This is a sample answer A", ///// Write the option 1 inside double quotes
                 b: "This is a sample answer B", ///// Write the option 2 inside double quotes
@@ -110,8 +110,38 @@
       correctAnswer: "b"                ///// Write the correct option inside double quotes
     },
 
-
-
+    {
+      question: "3. This is a sample question:",  ///// Write the question inside double quotes
+      answers: {
+                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
+                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                c: "This is a sample answer C",
+                d: "This is a sample answer D",
+            },
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
+    },
+    
+     {
+      question: "4. This is a sample question:",  ///// Write the question inside double quotes
+      answers: {
+                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
+                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                c: "This is a sample answer C",
+                d: "This is a sample answer D",
+            },
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
+    },                     
+    
+     {
+      question: "5. This is a sample question:",  ///// Write the question inside double quotes
+      answers: {
+                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
+                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                c: "This is a sample answer C",
+                d: "This is a sample answer D",
+            },
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
+    },                     
      ];
 
 
